fea019a5b2ab2fcbc7d6103033b8a0e5a4e215b2
Louis DEVIE <louisdevie.contact@gmail.com>
2021-11-25 10:24:40 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='kihara',
-    version='1.0.0',
+    version='0.1.0',
 
     author='Louis DEVIE',
     author_email='louisdevie.contact@gmail.com',
