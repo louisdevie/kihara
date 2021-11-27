348a1778a70cbf847824d7e0bacd554efc8c3686
Louis DEVIE <louisdevie.contact@gmail.com>
2021-11-27 17:19:25 +0100

    updated version number

--- kihara/version.py
+++ kihara/version.py
@@ -2,5 +2,5 @@
 Version info
 '''
 
-VERSION = (0, 1, 1)
+VERSION = (0, 2, 0)
 REPR = '.'.join([str(v) for v in VERSION])
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='kihara',
-    version='0.1.1',
+    version='0.2.2',
 
     author='Louis DEVIE',
     author_email='louisdevie.contact@gmail.com',
